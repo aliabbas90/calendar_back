60584bfc893e341bfc4826952db8a61e573f3ae2
aliabbas90 <ali@abbas.net>
2024-11-29 10:23:14 +0100

    Revert "Fix Firebase private key retrieval in database config."
    This reverts commit b67cf004349e30d6cbdcb6a7765fb4b9c18e14bb.

--- config/database.py
+++ config/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 firebase_credentials = {
     "type": "service_account",
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "token_uri": os.getenv("FIREBASE_TOKEN_URI")
 }
